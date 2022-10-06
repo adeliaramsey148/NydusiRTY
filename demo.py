92de36f9055296927b005d232bf39d8440adac03
adeliaramsey148 <104143460+adeliaramsey148@users.noreply.github.com>
2022-10-06 12:14:55 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,7 +8,6 @@
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
@@ -22,7 +21,6 @@ from pipeline_stable_diffusion_ait import StableDiffusionAITPipeline
 @click.command()
 @click.option("--token", default="", help="access token")
 @click.option("--prompt", default="A vision of paradise, Unreal Engine", help="prompt")
-@click.option(
     "--benchmark", type=bool, default=False, help="run stable diffusion e2e benchmark"
 )
 def run(token, prompt, benchmark):
